7084ec036d139e8272f4da460f21d98b03c6fadd
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-02 15:10:45 +0100

    Improve save button functionality. Add array of leads.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let inputBtn = document.getElementById("input-btn");
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
