a5d560e172c9130afa7bf7c95492108dd7c3e65a
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-04 01:21:02 +0100

    Add localStorage code. Add delete btn

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 let myLeads = [];
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(leadsFromLocalStorage)
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads();
+
+    console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads() {
